a5410af309a8d3a7b5957afe145b5825a25bf897
Tim Chevalier <chevalier@alum.wellesley.edu>
2016-01-26 14:07:15 -0800

    Code compiles but test fails

--- src/tada/tada.go
+++ src/tada/tada.go
@@ -1,13 +1,21 @@
+// +build !appengine
 package tada
 
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
+
+	"golang.org/x/net/context"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 )
 
 func init() {
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/getTodo", getTodoHandler)
+	http.HandleFunc("/putTodo", putTodoHandler)
 }
 
 type TodoItem struct {
@@ -15,21 +23,105 @@ type TodoItem struct {
 	dueDate     time.Time // Task due date
 }
 
-type TodoID int // database key / unique ID for a todo item
+type TodoID datastore.Key // database key / unique ID for a todo item
+
+type MaybeError interface {
+	isMaybeError()
+}
+
+type E string
+
+func (err E) isMaybeError()           {}
+func (t_item TodoItem) isMaybeError() {}
+func (t_id TodoID) isMaybeError()     {}
 
 // testing only
 var defaultTask = TodoItem{description: "test item", dueDate: time.Now()}
 
+// TODO: would really be better to statically require that Write returns an item and Read returns an ID
+
 // Takes a task description and a due date, returns a todo item ID
-func writeTodoItem(description string, dueDate time.Time) TodoID {
-	return 0 // stub
+func writeTodoItem(ctx context.Context, description string, dueDate time.Time) *MaybeError {
+	item := TodoItem{
+		description: description,
+		dueDate:     dueDate,
+	}
+	key, err := datastore.Put(ctx, datastore.NewIncompleteKey(ctx, "TodoItem", nil), &item)
+	var result = new(MaybeError)
+	if err != nil {
+		*result = E(err.Error())
+	} else {
+		*result = TodoID(*key)
+	}
+	return result
 }
 
 // Takes a todo item ID, returns a todo item
-func readTodoItem(itemID TodoID) TodoItem {
-	return defaultTask // stub
+func readTodoItem(ctx context.Context, itemID TodoID) *MaybeError {
+	var item *TodoItem
+	var err error
+	var result = new(MaybeError)
+	var key = new(datastore.Key)
+	*key = datastore.Key(itemID)
+	if err = datastore.Get(ctx, key, &item); err != nil {
+		*result = E(err.Error())
+	} else {
+		*result = *item
+	}
+	return (result)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Tada!")
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "I don't know what you want!")
+}
+
+func getTodoHandler(w http.ResponseWriter, r *http.Request) {
+	// create AppEngine context
+	ctx := appengine.NewContext(r)
+
+	// get id from request
+	id := r.FormValue("id")
+	// read a TodoItem from Datastore
+	i, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		http.Error(w, "You asked for a todo item that isn't a valid ID: "+id,
+			400)
+	} else {
+		k := datastore.NewKey(ctx, "Entity", "intID", i, nil)
+		item := readTodoItem(ctx, TodoID(*k))
+		respondWith(w, *item)
+	}
+}
+
+func putTodoHandler(w http.ResponseWriter, r *http.Request) {
+	// create AppEngine context
+	ctx := appengine.NewContext(r)
+
+	// get description from request
+	description := r.FormValue("description")
+	// get due date from request
+	dueDate := r.FormValue("dueDate")
+	d, err := time.Parse("2001-01-01", dueDate)
+	if err != nil {
+		http.Error(w, dueDate+" doesn't look like a valid date to me!",
+			400)
+	} else {
+		id := writeTodoItem(ctx, description, d)
+		respondWith(w, *id)
+	}
+}
+
+func respondWith(w http.ResponseWriter, result MaybeError) {
+	switch result.(type) {
+	case E:
+		// error
+		http.Error(w, string(result.(E)), 500)
+	case TodoID:
+		// we successfully wrote the item
+		fmt.Fprintf(w, "Successfully saved to-do item!")
+	case TodoItem:
+		// show the looked-up item
+		item := result.(TodoItem)
+		fmt.Fprintf(w, "item: %s due %d", item.description, item.dueDate)
+	}
 }
--- src/tada/tada_test.go
+++ src/tada/tada_test.go
@@ -3,12 +3,39 @@ package tada
 import (
 	"testing"
 	"time"
+
+	"google.golang.org/appengine/aetest"
 )
 
+func assertEquals(t *testing.T, expected, actual string) {
+	if expected != actual {
+		t.Errorf("Expected %s but was %s", expected, actual)
+	}
+}
+
 func TestReadAfterWrite(t *testing.T) {
-	dueDate := time.Date(2016, 2, 29, 13, 0, 0, 0, location.UTC)
-	itemId := writeTodoItem("finish writing these tests", dueDate)
-	theTodo := readTodoItem(itemId)
-	assertEquals(theTodo.description, "finish writing these tests")
-	assertEquals(theTodo.dueDate, dueDate)
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	dueDate := time.Date(2016, 2, 29, 13, 0, 0, 0, time.UTC)
+	itemId := writeTodoItem(ctx, "finish writing these tests", dueDate)
+	switch (*itemId).(type) {
+	case TodoID:
+		theTodo := readTodoItem(ctx, (*itemId).(TodoID))
+		switch (*theTodo).(type) {
+		case TodoItem:
+			theItem := (*theTodo).(TodoItem)
+			assertEquals(t, theItem.description, "finish writing these tests")
+			assertEquals(t, theItem.dueDate.String(), dueDate.String())
+		case E, TodoID:
+			t.Fatal("Expected read to return a todo item, got ", *theTodo)
+		}
+	case E, TodoItem:
+		t.Fatal("Expected write to return a todo ID, got something else")
+	}
+
+	defer done()
+
 }
